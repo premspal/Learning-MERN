230c80f3cef486764d7985257afc2e146c376bc6
prem <blackmango101@gmail.com>
2021-07-11 07:14:53 +0530

    MERN
    MERN

---
+++ mern-crud/client/src/components/Posts/Posts.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Post from './Post/Post'
+import useStyles from './styles'
+import { useSelector } from 'react-redux';
+
+const Posts = () => {
+    const posts = useSelector((state) => state.posts);
+    console.log(posts);
+
+    const classes=useStyles()
+    return (
+        <div>
+            <h1>Posts.js</h1>
+            <Post />
+            <Post />
+
+        </div>
+    )
+}
+
+export default Posts
